fc2e86c9bf75b0dd30c5cee28d663463dad7db78
git_saar@proton.me <SShtiler.didacticum.nl>
2022-10-05 15:03:49 +0200

    adding a speak function

---
+++ speech-function.ps1
@@ -0,0 +1,19 @@
+﻿
+
+
+function get-speech
+{
+param($WhatToSay, $SelectVoice)
+
+    Add-Type -AssemblyName system.speech
+    $Speak =New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $speak.SelectVoice($SelectVoice)
+    $Speak.Speak($WhatToSay)
+  
+    
+    
+
+}
+
+
+Get-speech -WhatToSay 'this function is great' -SelectVoice 'Microsoft Zira Desktop'
